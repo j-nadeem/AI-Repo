36c2e6c9ed9d7bd77e7a82443c892c1009b4b368
j-nadeem <118339211+j-nadeem@users.noreply.github.com>
2025-06-16 15:03:25 +0530

    Update app.js

--- Analog-clock/app.js
+++ Analog-clock/app.js
@@ -6,14 +6,14 @@ function getAngle(){
 
     //hours, minutes and seconds
     const today = new Date(); //Date object
-    let hours = today.getHours();
+    let hours = today.();
     let minutes = today.getMinutes();
     let seconds = today.getSeconds();
 
     // angle of rotation of hands 
     let hrHandRotation = 30*hours + minutes/2 + seconds/120;
     let minHandRotation = 6*minutes + seconds/10;
-    let secHandRotation = 6*seconds;
+    let secHandRotation = 6*secds;
 
     //now rotate the hands
     //adding translateX here otherwise only rotate works
